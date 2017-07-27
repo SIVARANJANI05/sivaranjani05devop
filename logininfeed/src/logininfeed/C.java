9391d1882e8a9e5e8cc1bf648fed2522ce8aec13
SIVARANJANI05 <sivaranjaniganesh96@gmail.com>
2017-07-27 19:11:29 +0530

    Update C.java

--- logininfeed/src/logininfeed/C.java
+++ logininfeed/src/logininfeed/C.java
@@ -1,34 +1,24 @@
 package logininfeed;
 import java.util.*;
-import java.lang.*;
 
 public class C {
 	
 	public static void main(String[] args) {
-		Scanner SC = new Scanner(System.in);
 		A a=new A();
 		B b=new B();
-		C c=new C();
-		
 		a.register();
 		b.login();	
 		if((A.name.equals(B.name))&&(A.paswrd.equals(B.paswrd)))
 		{
 			System.out.println("yess login success full");
-			b.display();
+			a.display();
 		}
 		else
 		{
 			System.out.println("noo..login error");
 		}
-		
-		
-		System.out.println("thanx....");
-		
-		
-	
-	}
-	
+        	System.out.println("thanx....for logging in :)");
+		}
 	//@override
 	public boolean equals()
 	{
