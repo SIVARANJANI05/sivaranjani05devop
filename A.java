332fff47565b26b3ff861e25a2fa3b3eef8c4484
SIVARANJANI05 <sivaranjaniganesh96@gmail.com>
2017-08-05 11:50:14 +0530

    thread basic 1_sum

---
+++ A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A extends Thread {
+	static int a,b,sum;
+	public void run()
+	{ 
+		Scanner SC=new Scanner(System.in);
+		System.out.println("enter a");
+		a=SC.nextInt();
+		System.out.println("enter b");
+		b=SC.nextInt();
+	}
+	
+	
+	
+
+}
